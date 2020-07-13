89b85616ee5aada4cad2829d3fbeb4e5f4619cf2
Raeed Asif <raeedasif@Raeeds-MacBook-Air.local>
2020-07-13 16:27:22 +0500

    Final commit

--- server.go
+++ server.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net"
-
-	covidlib "Capital-Stake-Test/lib"
+	"os"
+	"strings"
+	"time"
 )
 
 var (
-	reports = covidlib.Fetch("data/covid_final_data.csv")
+	reports = Fetch("data/covid_final_data.csv")
 )
 
 func main() {
@@ -53,6 +56,7 @@ func main() {
 	}
 }
 
+//'''Library for setting and hosting server'''
 //handleConnection :to handle incoming connection
 func handleConnection(conn net.Conn) {
 	defer func() {
@@ -61,7 +65,7 @@ func handleConnection(conn net.Conn) {
 		}
 	}()
 
-	if _, err := conn.Write([]byte("Connected...\nUsage: GET Date or Region\n")); err != nil {
+	if _, err := conn.Write([]byte("Connected...\nUsage:  Send Date or Region in JSON fromat\n")); err != nil {
 		log.Println("error writing:", err)
 		return
 	}
@@ -85,7 +89,7 @@ func handleConnection(conn net.Conn) {
 			continue
 		}
 
-		result := covidlib.Query(reports, param)
+		result := Query(reports, param)
 		if len(result) == 0 {
 			if _, err := conn.Write([]byte("Nothing found\n")); err != nil {
 				log.Println("failed to write:", err)
@@ -127,4 +131,77 @@ func parseCommand(cmdLine string) (param string) {
 	}
 
 	return
+}
+
+type covidstats struct {
+	Date                     string `json:"date"`
+	CumulativeTestPostive    string `json:"positive"`
+	CumulativeTestsPerformed string `json:"tests"`
+	Expired                  string `json:"expired"`
+	StillAdmitted            string `json:"admitted"`
+	Discharged               string `json:"discharged"`
+	Region                   string `json:"region"`
+}
+
+//'''Library for parsing and arranging data'''
+//Fetch :function to load and fetch csv file into table
+func Fetch(path string) []covidstats {
+	table := make([]covidstats, 0)
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err.Error())
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			panic(err.Error())
+		}
+		c := covidstats{
+			Date:                     DateFormat(row[4]),
+			CumulativeTestPostive:    row[2],
+			CumulativeTestsPerformed: row[3],
+			Expired:                  row[6],
+			StillAdmitted:            row[10],
+			Discharged:               row[5],
+			Region:                   row[9],
+		}
+		table = append(table, c)
+	}
+	return table
+}
+
+//Query :function to query the table and return results
+func Query(table []covidstats, filter string) []covidstats {
+	result := make([]covidstats, 0)
+	filter = strings.ToLower(filter)
+	for _, cov := range table {
+		region := strings.ToLower(cov.Region)
+		if cov.Date == filter || region == filter {
+			result = append(result, cov)
+		}
+	}
+	return result
+}
+
+//DateFormat: to format date to said format
+func DateFormat(DateString string) string {
+	var strDate = string(DateString[2])
+	switch {
+	case strDate == "-":
+		myDate, _ := time.Parse("02-Jan-06", DateString)
+		str := myDate.Format("2006-01-02")
+		return str
+	case strDate == "/":
+		myDate, _ := time.Parse("02/01/2006", DateString)
+		str := myDate.Format("2006-01-02")
+		return str
+	default:
+		return DateString
+	}
 }
